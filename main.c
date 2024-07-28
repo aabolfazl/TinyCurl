0c627172efe811ceb3d95f81959666560a469868
aabolfazl <aabolfazlit@gmail.com>
2024-07-28 23:40:27 +0300

    Update necessary arguments

--- main.c
+++ main.c
@@ -1,65 +1,57 @@
 #include <stdio.h>
-#include "arg_parse/arg_parse.h"
-#include <stdlib.h>
+#include "arg_parse.h"
 #include <string.h>
 
-
 static const char *const usages[] = {
-        "basic [options] [[--] args]",
-        "basic [options]",
-        NULL,
+    "tcurl [options] <url>",
+    NULL,
 };
 
-#define PERM_READ  (1<<0)
-#define PERM_WRITE (1<<1)
-#define PERM_EXEC  (1<<2)
-
 int main(int argc, const char **argv) {
-    int force = 0;
-    int test = 0;
-    int int_num = 0;
-    float flt_num = 0.f;
-    const char *path = NULL;
-    int perms = 0;
+    const char *url = NULL;
+    const char *output_file = NULL;
+    int verbose = 0;
+    int follow_redirects = 0;
+    const char *user_agent = NULL;
+    int timeout = 30;  // Default timeout in seconds
+    const char *request_method = "GET";
+
     struct argparse_option options[] = {
-            OPT_HELP(),
-            OPT_GROUP("Basic options"),
-            OPT_BOOLEAN('f', "force", &force, "force to do", NULL, 0, 0),
-            OPT_BOOLEAN('t', "test", &test, "test only", NULL, 0, 0),
-            OPT_STRING('p', "path", &path, "path to read", NULL, 0, 0),
-            OPT_INTEGER('i', "int", &int_num, "selected integer", NULL, 0, 0),
-            OPT_FLOAT('s', "float", &flt_num, "selected float", NULL, 0, 0),
-            OPT_GROUP("Bits options"),
-            OPT_BIT(0, "read", &perms, "read perm", NULL, PERM_READ, OPT_NONEG),
-            OPT_BIT(0, "write", &perms, "write perm", NULL, PERM_WRITE, 0),
-            OPT_BIT(0, "exec", &perms, "exec perm", NULL, PERM_EXEC, 0),
-            OPT_END(),
+        OPT_HELP(),
+        OPT_GROUP("Basic options"),
+        OPT_STRING('A', "user-agent", &user_agent, "Specify the User-Agent string", NULL, 0, 0),
+        OPT_INTEGER('m', "max-time", &timeout, "Maximum time allowed for the transfer", NULL, 0, 0),
+        OPT_STRING('X', "request", &request_method, "Specify request method to use", NULL, 0, 0),
+        OPT_END(),
     };
 
     struct argparse argparse;
     argparse_init(&argparse, options, usages, 0);
-    argparse_describe(&argparse, "\nA brief description of what the program does and how it works.",
-                      "\nAdditional description of the program after the description of the arguments.");
+    argparse_describe(&argparse,
+                      "\nA simple curl-like HTTP client.",
+                      "\nThis program sends HTTP requests and displays the responses.");
+
     argc = argparse_parse(&argparse, argc, argv);
-    if (force != 0)
-        printf("force: %d\n", force);
-    if (test != 0)
-        printf("test: %d\n", test);
-    if (path != NULL)
-        printf("path: %s\n", path);
-    if (int_num != 0)
-        printf("int_num: %d\n", int_num);
-    if (flt_num != 0)
-        printf("flt_num: %g\n", flt_num);
-    if (argc != 0) {
-        printf("argc: %d\n", argc);
-        int i;
-        for (i = 0; i < argc; i++) {
-            printf("argv[%d]: %s\n", i, *(argv + i));
-        }
+
+    // The remaining argument should be the URL
+    if (argc > 0) {
+        url = argv[0];
     }
-    if (perms) {
-        printf("perms: %d\n", perms);
+
+    if (url) {
+        printf("URL: %s\n", url);
+    }
+    if (output_file) {
+        printf("Output file: %s\n", output_file);
+    }
+    if (verbose) {
+        printf("Verbose mode: on\n");
+    }
+    if (follow_redirects) {
+        printf("Follow redirects: yes\n");
+    }
+    if (user_agent) {
+        printf("User-Agent: %s\n", user_agent);
     }
     return 0;
 }
