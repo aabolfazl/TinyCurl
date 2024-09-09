26b7dccad534a2b928bc37f1ee6f74313a46ba3b
Aliasghar112AB <abbasi,aliasghar1386@gmail.com>
2024-09-09 13:53:10 +0330

    Deleting unneccesary  things

--- main.c
+++ main.c
@@ -2,8 +2,7 @@
 #include "libs/arg_parse/arg_parse.h"
 #include <string.h>
 #include <sys/socket.h>
-//this command is for Lazygit test!
-//second test 
+ 
 static const char *const usages[] = {
     "tcurl [options] <url>",
     NULL,
