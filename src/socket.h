de3e163b9ad9e2499d5c6a11e61d40f9d3436c31
Aliasghar112AB <abbasi,aliasghar1386@gmail.com>
2024-11-03 23:48:13 +0330

    Function names changed

--- src/socket.h
+++ src/socket.h
@@ -6,13 +6,13 @@
 #define TCURL_SOCKET_H
 #include <netinet/in.h>
 
-void errorExit(const char *message);
-void parseUrl(const char *url, char *hostname, char *path);
-int isIpAddress(const char *hostname);
-int createSocket();
-void connectSocket(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
-void sendRequest(int sockfd, const char *request);
-void receiveResponse(int sockfd);
-void receiveHttpRequest(const char *method, const char *hostname, const char *path, const char *data, char *request);
+void error_exit(const char *message);
+void parse_url(const char *url, char *hostname, char *path);
+int is_ip_address(const char *hostname);
+int create_socket();
+void connect_socket(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
+void send_request(int sockfd, const char *request);
+void receive_response(int sockfd);
+void receive_http_request(const char *method, const char *hostname, const char *path, const char *data, char *request);
 
 #endif //TCURL_SOCKET_H
