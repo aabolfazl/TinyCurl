f6dee15da3d4389acea502c9b599a7bdf4c1b08a
Aliasghar112AB <abbasi,aliasghar1386@gmail.com>
2024-12-15 16:01:28 +0330

    POST method and data transfer between client and server added

--- src/socket.c
+++ src/socket.c
@@ -1,5 +1,6 @@
 #include "sys/socket.h"
 #include <arpa/inet.h>
+#include <bits/types/cookie_io_functions_t.h>
 #include <netdb.h>
 #include <netinet/in.h>
 #include <stdio.h>
@@ -7,69 +8,92 @@
 #include <sys/socket.h>
 #include "errno.h"
 #include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #define BUFFER_SIZE 4096
 
-int createSocket(){
-    int socketfd = socket(AF_INET,SOCK_STREAM,0);
-    if(socketfd < 0){
-        perror("Error for creating socket");
-        EXIT_FAILURE;
-    }
-    return socketfd;
+int is_ip_address(const char *hostname) {
+    struct sockaddr_in sa;
+    return inet_pton(AF_INET, hostname, &(sa.sin_addr)) == 1;
 }
 
-void socketConnect(int socket,const char *url){
+int socketCreateConnect(const char *hostname,const char *port){
     struct sockaddr_in server_addr;
-    struct hostent *server;
+    int sockfd;
 
-    server = gethostbyname(url);
-    if (server == NULL) {
-        fprintf(stderr, "Error: No such host\n");
-        exit(EXIT_FAILURE);
+    if (is_ip_address(hostname)) {
+        server_addr.sin_family = AF_INET;
+        server_addr.sin_port = htons(atoi(port));
+        if (inet_pton(AF_INET, hostname, &server_addr.sin_addr) <= 0) {
+            perror("Invalid ip address");
+            exit(EXIT_FAILURE);
+        }
+        sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        if (sockfd < 0) {
+            perror("Socket creation failed");
+            exit(EXIT_FAILURE);
+        }
+         int socketconnect = connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+            
+            if(socketconnect < 0){
+                close(sockfd);
+                perror("socket Connection failed");
+                exit(EXIT_FAILURE);
+            }
+    } else {
+        struct addrinfo hints, *res;
+        memset(&hints, 0, sizeof hints);
+        hints.ai_family = AF_INET;
+        hints.ai_socktype = SOCK_STREAM;
+        if (getaddrinfo(hostname, port, &hints, &res) != 0) {
+            perror("getaddrinfo failed");
+            exit(EXIT_FAILURE);
+        }
+        sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+        if (sockfd < 0) {
+            perror("Socket creation failed");
+            exit(EXIT_FAILURE);
+        }
+        if (connect(sockfd, res->ai_addr, res->ai_addrlen) < 0) {
+            close(sockfd);
+            freeaddrinfo(res);
+            perror("Connection failed");
+            exit(EXIT_FAILURE);
+        }
+        freeaddrinfo(res);
     }
-     memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(80);
-    memcpy(&server_addr.sin_addr.s_addr, server->h_addr, server->h_length);
-    
-
-    int socketConnect = connect(socket,(struct sockaddr *)&server_addr,sizeof(server_addr));
-    if(socketConnect < 0){
-        perror("Error while connecting");
-        exit(EXIT_FAILURE);
+    return sockfd ;
+}
+void httpRequest(int socketfd, const char *method, const char *hostname,const char *path, const char *data){
+      char request[BUFFER_SIZE];
+    if (strcasecmp(method, "POST") == 0) {
+        snprintf(request, sizeof(request),
+                 "POST %s HTTP/1.1\r\n"
+                 "Host: %s\r\n"
+                 "Content-Type: application/x-www-form-urlencoded\r\n"
+                 "Content-Length: %ld\r\n"
+                 "Connection: close\r\n\r\n"
+                 "%s",
+                 path, hostname, strlen(data), data);
+    } else if (strcasecmp(method, "GET") == 0) {
+        snprintf(request, sizeof(request),
+                 "GET %s HTTP/1.1\r\n"
+                 "Host: %s\r\n"
+                 "Connection: close\r\n\r\n",
+                 path, hostname);
     }
+    int sendRequest = send(socketfd,request, strlen(request),0);
 }
 
-void socketSend(int socketfd, const char *url, const char *method){
-    char buffer[BUFFER_SIZE];
-    char message[BUFFER_SIZE];
-
-    if((strncmp(method, "GET", 3)) == 0){   
-         snprintf(message, sizeof(message),
-             "GET / HTTP/1.1\r\n"
-             "Host: %s\r\n"
-             "Connection: close\r\n\r\n",
-              url);
-    }else if(strncmp(method, "POST", 4) == 0){
-        printf("Unsupported Method!\n");
+void recvRequest(int sockfd){
+    char response[BUFFER_SIZE];
+    ssize_t recievedResponse;
+    recievedResponse = recv(sockfd,response,sizeof(response) - 1,0);
+    if(recievedResponse < 0){
+        perror("recv failed!");
         exit(EXIT_FAILURE);
     }
-
-    int sendMessage = send(socketfd, message, strlen(message), 0);
-    if(sendMessage < 0){
-        perror("Error while sending message");
-        exit(EXIT_FAILURE);
-    }
-}
-
-void socketRecv(int socketfd){
-    char buffer[2056];
-    int socket_recv = recv(socketfd, buffer, sizeof(buffer) -1, 0);
-    if(socket_recv < 0){
-        perror("Error for receiving data");
-        exit(EXIT_FAILURE);
-    }
-    buffer[socket_recv] = '\0';
-    printf("%s\n",buffer); 
+    response[recievedResponse] = '\0';
+    printf("%s",response);
 }
